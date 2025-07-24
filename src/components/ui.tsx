3390527a7c9787c50d9f20e05024808a50a60d8e
alitaiz <127329961+alitaiz@users.noreply.github.com>
2025-07-24 23:41:19 +0700

    Update ui.tsx

--- src/components/ui.tsx
+++ src/components/ui.tsx
@@ -11,7 +11,7 @@ export const PawPrintIcon = ({ className }: { className?: string }) => (
 
 
 export const HeartIcon = ({ className }: { className?: string }) => (
-  <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+  <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
     <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
   </svg>
 );
