7e822ffb4550f0445ee725b119484f6cd1de78ce
alitaiz <127329961+alitaiz@users.noreply.github.com>
2025-07-24 21:35:25 +0700

    Update index.js

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import fetch from 'node-fetch';
 
 // Load environment variables from .env file
 dotenv.config();
